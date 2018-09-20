4dcca1f60f663a0e68464d0287eaca30333a7c3e
MaximeBouton <maxim.bouton@gmail.com>
2018-09-20 15:31:20 -0700

    RNN training script

---
+++ training_scripts/RNNFiltering/train.sh
@@ -0,0 +1,8 @@
+nohup julia bagging_training.jl --seed 10 --resume > nn_10.jodhpur.out &
+nohup julia bagging_training.jl --seed 20 --resume > nn_20.jodhpur.out &
+nohup julia bagging_training.jl --seed 30 --resume > nn_30.jodhpur.out &
+nohup julia bagging_training.jl --seed 40 --resume > nn_40.jodhpur.out &
+nohup julia bagging_training.jl --seed 50 --resume > nn_50.jodhpur.out &
+
+
+
