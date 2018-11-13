736b9bedc01e88abd36f0823353baf453bedab03
MaximeBouton <maxim.bouton@gmail.com>
2018-11-13 11:03:30 -0800

    lower proba of appearance

--- training_scripts/pedcar_vi.jl
+++ training_scripts/pedcar_vi.jl
@@ -3,6 +3,11 @@ using Random
 N_PROCS=56
 addprocs(N_PROCS)
 rng = MersenneTwister(1)
+using DiscreteValueIteration
+using AutomotivePOMDPs
+using PedCar
+using LocalApproximationValueIteration
+using MDPModelChecking
 @everywhere begin 
     using Printf
     using POMDPs, POMDPModelTools, DiscreteValueIteration
@@ -20,8 +25,8 @@ rng = MersenneTwister(1)
     end
 end # @everywhere
 
-include("src/masking.jl")
-include("src/util.jl")
+include("../src/masking.jl")
+include("../src/util.jl")
 
 
 @everywhere params = UrbanParams(nlanes_main=1,
@@ -31,7 +36,7 @@ include("src/util.jl")
                      stop_line = 22.0)
 @everywhere env = UrbanEnv(params=params);
 
-@everywhere mdp = PedCar.PedCarMDP(env=env, pos_res=2.0, vel_res=2.0, ped_birth=0.7, car_birth=0.7, ped_type=VehicleDef(AgentClass.PEDESTRIAN, 1.0, 3.0))
+@everywhere mdp = PedCar.PedCarMDP(env=env, pos_res=2.0, vel_res=2.0, ped_birth=0.1, car_birth=0.1, ped_type=VehicleDef(AgentClass.PEDESTRIAN, 1.0, 3.0))
 @everywhere init_transition!(mdp)
 # reachability analysis
 mdp.collision_cost = 0.
@@ -39,7 +44,7 @@ mdp.γ = 1.
 mdp.goal_reward = 1.
 
 solver = ParallelValueIterationSolver(n_procs=N_PROCS, max_iterations=20, belres=1e-4, include_Q=true, verbose=true)
-policy_file = "pedcar_utility.jld2"
+policy_file = "pedcar_utility_low.jld2"
 #policy = ValueIterationPolicy(mdp, include_Q=true)
 if isfile(policy_file)
   println("Loading policy file $policy_file")
@@ -52,7 +57,7 @@ save(policy_file, "util", policy.util, "qmat", policy.qmat, "pol",policy.policy)
 #bson(policy_file, util=policy.util, qmat=policy.qmat, pol=policy.policy)
 #bson("pedcar_policy.bson", policy=policy)
 
-threshold = 0.999
+threshold = 0.99
 mdp.collision_cost = -1.0
 mask = SafetyMask(mdp, policy, threshold);
 rand_pol = MaskedEpsGreedyPolicy(mdp, 1.0, mask, rng);
@@ -65,7 +70,7 @@ solver.init_util = policy.util
 policy = solve(solver, mdp) # resume
 
 
-threshold = 0.999
+threshold = 0.99
 mdp.collision_cost = -1.0
 mask = SafetyMask(mdp, policy, threshold);
 rand_pol = MaskedEpsGreedyPolicy(mdp, 1.0, mask, rng);
